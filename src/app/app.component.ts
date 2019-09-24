ee728beea7278e6932afe285049ffd3fea322fd7
gschafra <gschafra@web.de>
2019-09-24 19:51:17 +0200

    Removed PrimeNG and HelloComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { Component } from '@angular/core';
-import { SelectItem } from 'primeng/api';
+import { Component } from '@angular/core';,
 import { FormBuilder, FormGroup, FormGroupDirective, Validators } from '@angular/forms';
 
 @Component({
@@ -8,26 +7,4 @@ import { FormBuilder, FormGroup, FormGroupDirective, Validators } from '@angular
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  options: SelectItem[] = [];
-  formGroup: FormGroup;
-  model: any;
-
-  constructor(
-		private readonly formBuilder: FormBuilder
-	)
-	{
-	}
-
-  ngOnInit() {
-    this.options = [
-            {label: 'Option1', value: 1, icon: 'fa-user'},
-            {label: 'Option2', value: 2, icon: 'fa-user'},
-            {label: 'Option3', value: 3, icon: 'fa-user'},
-        ];
-
-    this.formGroup = this.formBuilder.group({
-			item: [null, Validators.required]
-		});
-
-  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { DropdownModule } from 'primeng/dropdown';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';,
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { AppComponent } from './app.component';,
 import { BlogService } from './services/blog.service';
 import { AuthorService } from './services/author.service';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, ReactiveFormsModule, BrowserAnimationsModule, DropdownModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ BrowserModule, FormsModule, ReactiveFormsModule, BrowserAnimationsModule ],
+  declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ],
   providers: [ BlogService, AuthorService ]
 })
